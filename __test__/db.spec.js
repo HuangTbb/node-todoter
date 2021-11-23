0eb00224e9fc45d258222e99a7a9735ab5d1bd5c
hjy <5336772+huangtobe@user.noreply.gitee.com>
2021-11-23 19:03:18 +0800

    添加测试文件

---
+++ __test__/db.spec.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const db = require('../db')
+jest.mock('fs');
+describe('db', ()=>{
+  afterEach(()=>{
+    fs.clearMocks()
+  })
+  it('can read', async ()=> {
+    const data = [{title:'hi',done:true}]
+    fs.setReadFileMock('/xxx', null, JSON.stringify(data))
+    const list = await db.read('/xxx')
+    expect(list).toStrictEqual(data)
+  })
+  it('can write', async ()=>{
+    let fakeFile
+    fs.setWriteFileMock('/yyy', (path, data,callback)=> {
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title: '看美女',done:true},{title: '看帅哥', done:true}]
+    await  db.write(list, '/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list)+'\n')
+  })
+})
